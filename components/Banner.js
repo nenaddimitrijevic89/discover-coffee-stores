f3a6cc1d191a129625778cf8f51541b7ab67b862
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-07-05 21:47:42 +0200

    Add banner component

---
+++ components/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div>
+      <p>Coffee stores</p>
+      <button>Search</button>
+    </div>
+  );
+};
+
+export default Banner;
