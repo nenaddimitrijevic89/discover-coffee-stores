32e1c96a843734ead79cfb00fa7847ca9b36f5f6
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-07-06 22:15:16 +0200

    Add card component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
-import Banner from "../components/Banner";
+
 import styles from "../styles/Home.module.css";
+import coffeeStores from "../data/coffee-stores.json";
+
+import Banner from "../components/Banner/Banner";
+import Card from "../components/Card/Card";
 
 export default function Home() {
   const handleOnBannerBtnClick = () => {
@@ -21,7 +25,22 @@ export default function Home() {
           handleOnClick={handleOnBannerBtnClick}
         />
         <div className={styles.heroImage}>
-          <Image src="/static/hero-image.png" width={700} height={400} alt="hero"/>
+          <Image
+            src="/static/hero-image.png"
+            width={700}
+            height={400}
+            alt="hero"
+          />
+        </div>
+        <div className={styles.cardLayout}>
+          {coffeeStores.map((store) => (
+            <Card
+              key={store.id}
+              name={store.name}
+              imgUrl={store.imgUrl}
+              href={`/coffee-store/${store.id}`}
+            />
+          ))}
         </div>
       </main>
     </div>
