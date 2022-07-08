be4810f0b4d31cb3bb2110f6677be7826ecc6bf3
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-07-08 18:40:25 +0200

    WIP: Add getStaticPaths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,17 +1,47 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-const CoffeeStore = () => {
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+const CoffeeStore = (props) => {
   const router = useRouter();
   const { id } = router.query;
+  
+  if (router.isFallback) {
+    return <div>Loading....</div>;
+  }
+
   return (
     <div>
       <p>Coffee Store {id}</p>
       <Link href="/">
         <a>go back</a>
       </Link>
+      <p>{props.coffeeStore.address}</p>
+      <p>{props.coffeeStore.name}</p>
     </div>
   );
 };
+
+export function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(
+        (store) => store.id.toString() === params.id
+      ),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    // paths: coffeeStoresData.map((store) => ({
+    //   params: { id: store.id.toString() },
+    // })),
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    fallback: true,
+  };
+}
 
 export default CoffeeStore;
