73768ad60b266ad1092dadbb644c15161efa4c0e
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-07-31 13:27:54 +0200

    Setup Airtable and createStore API

---
+++ pages/api/createCoffeeStore.js
@@ -0,0 +1,40 @@
+const Airtable = require("airtable");
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE_KEY
+);
+
+const table = base("coffee-stores");
+
+console.log({ table });
+
+const createCoffeeStore = async (req, res) => {
+  if (req.method === "POST") {
+    try {
+      //find
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id="0"`,
+        })
+        .firstPage();
+
+      console.log({ findCoffeeStoreRecords });
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = findCoffeeStoreRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json(records);
+      } else {
+        //create
+        res.json({ message: "create a record" });
+      }
+    } catch (err) {
+      console.log("Error finding store", err);
+      res.status(500);
+      res.json({ message: "Error finding store", err });
+    }
+  }
+};
+
+export default createCoffeeStore;
