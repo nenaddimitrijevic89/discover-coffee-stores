416b9538d05a1751d81aca0e751ec214c38e23c4
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-07-31 21:06:35 +0200

    Refactor createCoffeeStore API

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,31 +1,20 @@
-const Airtable = require("airtable");
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_KEY
-);
-
-const table = base("coffee-stores");
-
-console.log({ table });
+import { table, getMinifiedRecords } from "../../lib/airtable";
 
 const createCoffeeStore = async (req, res) => {
-  const { id, name, address, neighbourhood, voting, imgUrl } = req.body;
   if (req.method === "POST") {
+    const { id, name, address, neighborhood, voting, imgUrl } = req.body;
+
     try {
       //find
       if (id) {
         const findCoffeeStoreRecords = await table
           .select({
-            filterByFormula: `id=${id}`,
+            filterByFormula: `id="${id}"`,
           })
           .firstPage();
 
-        console.log({ findCoffeeStoreRecords });
         if (findCoffeeStoreRecords.length !== 0) {
-          const records = findCoffeeStoreRecords.map((record) => {
-            return {
-              ...record.fields,
-            };
-          });
+          const records = getMinifiedRecords(findCoffeeStoreRecords);
           res.json(records);
         } else {
           //create
@@ -36,18 +25,14 @@ const createCoffeeStore = async (req, res) => {
                   id,
                   name,
                   address,
-                  neighbourhood,
+                  neighborhood,
                   voting,
                   imgUrl,
                 },
               },
             ]);
 
-            const records = createRecords.map((record) => {
-              return {
-                ...record.fields,
-              };
-            });
+            const records = getMinifiedRecords(createRecords);
             res.json({ message: "record created", records });
           } else {
             res.status(400);
@@ -59,9 +44,9 @@ const createCoffeeStore = async (req, res) => {
         res.json({ message: "Id is missing!" });
       }
     } catch (err) {
-      console.log("Error finding store", err);
+      console.error("Error creating or finding store", err);
       res.status(500);
-      res.json({ message: "Error finding store", err });
+      res.json({ message: "Error creating or finding store", err });
     }
   }
 };
