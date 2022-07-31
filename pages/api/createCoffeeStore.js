9ddfba4e17197744f6aa8f06bfb3cbddf75140e2
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-07-31 14:14:46 +0200

    Implement createCoffeeStore API

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -8,26 +8,55 @@ const table = base("coffee-stores");
 console.log({ table });
 
 const createCoffeeStore = async (req, res) => {
+  const { id, name, address, neighbourhood, voting, imgUrl } = req.body;
   if (req.method === "POST") {
     try {
       //find
-      const findCoffeeStoreRecords = await table
-        .select({
-          filterByFormula: `id="0"`,
-        })
-        .firstPage();
+      if (id) {
+        const findCoffeeStoreRecords = await table
+          .select({
+            filterByFormula: `id=${id}`,
+          })
+          .firstPage();
 
-      console.log({ findCoffeeStoreRecords });
-      if (findCoffeeStoreRecords.length !== 0) {
-        const records = findCoffeeStoreRecords.map((record) => {
-          return {
-            ...record.fields,
-          };
-        });
-        res.json(records);
+        console.log({ findCoffeeStoreRecords });
+        if (findCoffeeStoreRecords.length !== 0) {
+          const records = findCoffeeStoreRecords.map((record) => {
+            return {
+              ...record.fields,
+            };
+          });
+          res.json(records);
+        } else {
+          //create
+          if (name) {
+            const createRecords = await table.create([
+              {
+                fields: {
+                  id,
+                  name,
+                  address,
+                  neighbourhood,
+                  voting,
+                  imgUrl,
+                },
+              },
+            ]);
+
+            const records = createRecords.map((record) => {
+              return {
+                ...record.fields,
+              };
+            });
+            res.json({ message: "record created", records });
+          } else {
+            res.status(400);
+            res.json({ message: "Name is missing!" });
+          }
+        }
       } else {
-        //create
-        res.json({ message: "create a record" });
+        res.status(400);
+        res.json({ message: "Id is missing!" });
       }
     } catch (err) {
       console.log("Error finding store", err);
