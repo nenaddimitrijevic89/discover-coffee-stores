8c37fbcd21f0e636e5c115639b31c823810fd1de
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-08-01 15:58:46 +0200

    Create getCoffeeStoreById API

---
+++ pages/api/getCoffeeStoreById.js
@@ -0,0 +1,25 @@
+import { findRecordByFilter } from "../../lib/airtable";
+
+const getCoffeeStoreById = async (req, res) => {
+  const { id } = req.query;
+
+  try {
+    if (id) {
+      const records = await findRecordByFilter(id)
+
+      if (records.length !== 0) {
+        res.json(records);
+      } else {
+        res.json({ message: `Id could not be found!` });
+      }
+    } else {
+      res.status(400);
+      res.json({ message: "Id is missing!" });
+    }
+  } catch (err) {
+    res.status(500);
+    res.json({ message: "Something went wrong!", err });
+  }
+};
+
+export default getCoffeeStoreById;
