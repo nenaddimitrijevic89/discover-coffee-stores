0029130f484f38a3a71a11a3df3dd1810dacbbdb
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-08-04 20:05:36 +0200

    Fix build error

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home(props) {
       }
     }
     setCoffeeStoresByLocation();
-  }, [latLong]);
+  }, [latLong, dispatch]);
 
   const handleOnBannerBtnClick = () => {
     handleTrackLocation();
